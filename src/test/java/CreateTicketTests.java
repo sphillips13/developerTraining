36a3c058250b6a8cf2b4a6981b748530d414073e
Shawn <phi13025@byui.edu>
2021-01-14 08:46:45 -0700

    Removed code for the test file

--- src/test/java/CreateTicketTests.java
+++ src/test/java/CreateTicketTests.java
@@ -17,19 +17,13 @@ import td.api.Ticket;
 public class CreateTicketTests {
 
     // TODO: Create CreatTicket, DeveloperTicketIDs, and Custom Attribute Objects
-    private CreateTicket testCreateTicket = new CreateTicket();
-    private Ticket testDeveloperTicket = new Ticket();
-    private DeveloperTicketIDs iDs = new DeveloperTicketIDs();
-    private CustomAttribute customAttribute = new CustomAttribute();
 
     @BeforeAll
     public void setup() {
 
         // TODO: Set your Ticket Object equal to buildDeveloperTicket()
-        testDeveloperTicket = testCreateTicket.buildDeveloperTicket();
 
         // TODO: Set your CustomAttribute Ticket
-        customAttribute = new CustomAttribute(iDs.DEVELOPER_TAG_ID, Integer.toString(iDs.DEVELOPER_TAG_VALUE_HTML_ID));
 
     }
 
@@ -38,19 +32,9 @@ public class CreateTicketTests {
 
         // TODO: Assert that each value set is correct
         // Default Attributes
-        assert(testDeveloperTicket.getFormId() == 3300);
-        assert(testDeveloperTicket.getTitle() == "Developer Training Ticket");
-        assert(testDeveloperTicket.getRequestorUid() == "e578bb85-0041-e511-80d1-005056ac5ec6");
-        assert(testDeveloperTicket.getDescription() == "R2S2 Member Test Ticket");
-        assert(testDeveloperTicket.getStatusId() == 979);
-        assert(testDeveloperTicket.getAppId() == 63);
-        assert(testDeveloperTicket.getTypeId() == 857);
-        assert(testDeveloperTicket.getAccountId() == 553);
-        assert(testDeveloperTicket.getPriorityId() == 21);
 
         // TODO: Assert that your CustomAttribute is in your Ticket Object
         // Custom Attributes
-        assert(testDeveloperTicket.getAttributes().contains(customAttribute));
 
     }
 }
